9ff7c706ab8f8dd55fab3e49d612df45f4c6ec97
Lucas Cimon <lucas.cimon@gmail.com>
2019-01-08 15:15:37 +0100

    TravisCI minor fix

--- scavengers/gen_pdf.sh
+++ scavengers/gen_pdf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -o pipefail -o errexit -o nounset -o xtrace
 cd $(dirname ${BASH_SOURCE[0]})
-OUT_FILE=${1:-adj-scavengers.pdf}
-puppeteer print adj-scavengers.html tmp-$OUT_FILE
-pdfjam --no-tidy tmp-$OUT_FILE --nup 2x1 --landscape --outfile $OUT_FILE
-rm tmp-$OUT_FILE
+IN_FILE=${1:-adj-scavengers.html}
+OUT_FILE=${2:-adj-scavengers.pdf}
+puppeteer print ${IN_FILE} tmp-${OUT_FILE}
+pdfjam --no-tidy tmp-${OUT_FILE} --nup 2x1 --landscape --outfile ${OUT_FILE}
+rm tmp-${OUT_FILE}
