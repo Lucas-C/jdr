718b21c6e4f540b7a6acfdc5d79643cdac8de466
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-07-21 20:40:45 +0200

    OriMushi minor improvements

--- wake-III-area/avatars/scrape_q3df_models.py
+++ wake-III-area/avatars/scrape_q3df_models.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
+import logging, re, requests
 from bs4 import BeautifulSoup
-import re, requests
+from http.client import HTTPConnection
 from random import randrange
 
+# logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
+# HTTPConnection.debuglevel = 2
 
 RESULTS_PER_PAGE = 10  # smaller => less HTML to parse
 
+session = requests.session()  # make things a lot faster by re-using the same PHPSESSID cookie
+
 
 def get_model_count():
-    resp = requests.get('https://ws.q3df.org/models/')
+    resp = session.get('https://ws.q3df.org/models/')
     resp.raise_for_status()
     match = re.search(r" (\d+) results", resp.text)
     return int(match.group(1))
@@ -16,7 +21,7 @@ def get_model_count():
 def get_model_href(model_index):
     page_id = model_index // RESULTS_PER_PAGE
     url = f'https://ws.q3df.org/models/?model=&page={page_id}&show={RESULTS_PER_PAGE}'
-    resp = requests.get(url)
+    resp = session.get(url)
     resp.raise_for_status()
     soup = BeautifulSoup(resp.text, 'html.parser')
     items = soup.find_all(class_='models_modelitem')
@@ -27,7 +32,7 @@ def get_model_href(model_index):
 
 def get_img_urls(model_href):
     url = 'https://ws.q3df.org' + model_href
-    resp = requests.get(url)
+    resp = session.get(url)
     resp.raise_for_status()
     soup = BeautifulSoup(resp.text, 'html.parser')
     details_container = soup.find(id='modeldetails_container')
@@ -43,5 +48,11 @@ def get_img_urls(model_href):
 if __name__ == '__main__':
     model_count = get_model_count()
     model_index = randrange(model_count)
+    print(f'{model_index=}')
     model_href = get_model_href(model_index)
-    print(get_img_urls(model_href))
+    img_urls = get_img_urls(model_href)
+    for img_url in img_urls.values():
+        resp = session.get(img_url, stream=True)
+        resp.raise_for_status()
+        with open(img_url[27:], 'wb') as img_file:
+            img_file.write(resp.raw.read())
