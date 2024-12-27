11b03159c35028c42850074fee3914de3f666c63
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2024-12-27 13:55:08 +0100

    WIP OriMushi

---
+++ OriMushi/README.md
@@ -0,0 +1,7 @@
+Dernière version du PDF: [OriMushi.pdf](https://lucas-c.github.io/jdr/OriMushi/OriMushi.pdf)
+
+Pour regénérer le PDF avec Python :
+```
+pip install mistletoe weasyprint
+./md2pdf.py
+```
--- OriMushi/gen_illus_pdf.py
+++ OriMushi/gen_illus_pdf.py
@@ -11,7 +11,7 @@ pdf.set_margin(10)
 
 def add_image(path):
     x, y = pdf.x, pdf.y
-    info = pdf.image(path, h=IMG_SIZE, w=IMG_SIZE, keep_aspect_ratio=True)
+    info = pdf.image("cc-imgs/" + path, h=IMG_SIZE, w=IMG_SIZE, keep_aspect_ratio=True)
     if x + info.rendered_width < pdf.w / 2:
         pdf.x = pdf.w / 2
         pdf.y = y
@@ -21,46 +21,72 @@ def add_image(path):
 
 # PJs :
 pdf.add_page()
-add_image("cc-imgs/TCP-Human-4-cc-by.jpg")
-add_image("cc-imgs/elf-cc0.png")
-add_image("cc-imgs/Pirate-ninja-cc0.svg")
-add_image("cc-imgs/quick_samurai_sketch_by_hidanbasher_cc-by.jpg")
+add_image("Aeshi_the_marvelous_maiden_gardevoir_by_amazingsphelon_cc-by-sa.jpg")
+add_image("Miashe_blare_the_fiery_fighter_blaziken_by_amazingsphelon_cc-by-sa.jpg")
+add_image("OC-Naiya-Creswell_by_amazingsphelon_cc-by-sa.jpg")
+add_image("OC-Raid-Northgain_by_amazingsphelon_cc-by-sa.jpg")
 
 pdf.add_page()
-add_image("cc-imgs/Alopex_redesign_concept_by_beevirus_cc-by-nc-sa.jpg")
-add_image("cc-imgs/Human_Alopex_by_beevirus_cc-by-nc-sa.jpg")
-add_image("cc-imgs/ryzom-tryker-femme-cc-by-sa.jpg")
-add_image("cc-imgs/ryzom-tryker-homme-cc-by-sa.jpg")
+add_image("BR-Portrait-Erika-POKEMON_by_amazingsphelon_cc-by-sa.jpg")
+add_image("OC_raffle_Rianna_by_amazingsphelon_cc-by-sa.jpg")
+add_image("Charlenne_by_amazingsphelon_cc-by-sa.jpg")
+add_image("Battle_Rehime_Sol_Badguy_by_amazingsphelon_cc-by-sa.jpg")
 
 pdf.add_page()
-add_image("cc-imgs/Breeze-in-the-Forest_by_tysontan_cc-by-sa.jpg")
-add_image("cc-imgs/kitty-cc0.jpg")
-add_image("cc-imgs/Lion-Swordsman-by-hijodelopio-cc-by.png")
-add_image("cc-imgs/Shinobi-Saipat-by-hijodelopio-cc-by.jpg")
+add_image("Ashitaka_Princess_Mononoke_by_amazingsphelon_cc-by-sa.png")
+add_image("Hanzo_Hatoori_The_fierceful_ninja_ninjask_by_amazingsphelon_cc-by-sa.jpg")
+pdf.image("cc-imgs/Streets-on-Hazards-II-Martial-Arts-Attires_by_amazingsphelon_cc-by-sa.jpg", h=IMG_SIZE, w=pdf.epw, keep_aspect_ratio=True)
+
+pdf.add_page()
+add_image("espio_the_chameleon_gijinka_cc-by.png")
+add_image("jet_the_hawk_gijinka_by_rosytoonz_cc-by.png")
+add_image("storm_the_albatross_gijinka_by_rosytoonz_cc-by.png")
+
+pdf.add_page()
+add_image("Alopex_redesign_concept_by_beevirus_cc-by-nc-sa.jpg")
+add_image("Human_Alopex_by_beevirus_cc-by-nc-sa.jpg")
+add_image("quick_samurai_sketch_by_hidanbasher_cc-by.png")
+
+pdf.add_page()
+add_image("ryzom-tryker-femme-cc-by-sa.jpg")
+add_image("ryzom-tryker-homme-cc-by-sa.jpg")
+add_image("elf-cc0.png")
+add_image("pixabay-ninja-girl.png")
 
 # PNJs :
 pdf.add_page()
-add_image("cc-imgs/Dakuan_by_daudiomultimedia_cc-by-nc.png")
-add_image("cc-imgs/TCP-Manimal-3-cc-by.jpg")
-add_image("cc-imgs/Kenku-by-ubergank-cc-by-sa.jpg")
-add_image("cc-imgs/elias_ainsworth_the_ancient_magus_bride_by_amgr99_cc-by-sa.jpg")
+add_image("Dakuan_by_daudiomultimedia_cc-by-nc.png")
+add_image("TCP-Manimal-3-cc-by.jpg")
+add_image("Kenku-by-ubergank-cc-by-sa.jpg")
+add_image("komainu_raican_by_hijodelopio_cc-by.png")
+
+pdf.add_page()
+add_image("kuma_by_hijodelopio_cc-by.png")
+add_image("Lion-Swordsman-by-hijodelopio-cc-by.png")
+add_image("Shinobi-Saipat-by-hijodelopio-cc-by-noBg.png")
+add_image("kitty-cc0.png")
+
+pdf.add_page()
+add_image("Mind-Temple-Character_by_assumzaek_cc-by-nc-sa.png")
+add_image("elias_ainsworth_the_ancient_magus_bride_by_amgr99_cc-by-sa.jpg")
+add_image("Pirate-ninja-cc0.svg")
+
+# Lieux
+pdf.add_page()
+add_image("fisherman_s_house_by_assumzaek_cc-by-nc-sa.jpg")
+add_image("village_by_assumzaek_cc-by-nc-sa.jpg")
 
 # Objets spéciaux :
 pdf.add_page()
-add_image("cc-imgs/magic_circle_2_by_nnao_cc-by-nc-sa.jpg")
-add_image("cc-imgs/western_kit_by_fernand0fc_cc-by.jpg")
-add_image("cc-imgs/Grappling_hook-cc0.png")
-add_image("cc-imgs/Katanas_by_Halibutt_cc-by.svg")
+add_image("Katanas_by_Halibutt_cc-by.svg")
+add_image("Grappling_hook-cc0.png")
+pdf.ln(20)
+add_image("shurikens-cc0.png")
+add_image("Bow-and-Arrow-cc0.png")
 
 pdf.add_page()
-add_image("cc-imgs/asian_paper_umbrella-cc0.jpg")
-add_image("cc-imgs/TCP-Armored-3-cc-by.jpg")
-add_image("cc-imgs/asian-fan-with-a-map-cc0.svg")
-add_image("cc-imgs/shuriken-cc0.png")
-
-pdf.add_page()
-add_image("cc-imgs/red-chinese-scroll-cc0.svg")
-add_image("cc-imgs/Bow-and-Arrow-cc0.png")
+add_image("red-chinese-scroll-cc0.svg")
+add_image("asian-fan-with-a-map-cc0.svg")
 pdf.image("cc-imgs/Woodland-Dragon_by_flaming-anubis_cc-by-sa.jpg", h=IMG_SIZE, w=pdf.epw, keep_aspect_ratio=True)
 
 out_filepath = "OriMushi-illustrations.pdf"
