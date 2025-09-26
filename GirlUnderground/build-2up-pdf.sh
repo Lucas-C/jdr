7174a208f01d6a33b04411a97590315a378f437f
Lucas-C <Lucas-C@users.noreply.github.com>
2025-09-26 11:33:36 +0000

    deploy: ef96803c19622680760fdcf05f4438c3448cc1cd

--- GirlUnderground/build-2up-pdf.sh
+++ GirlUnderground/build-2up-pdf.sh
@@ -6,5 +6,11 @@ cd $(dirname ${BASH_SOURCE[0]})
 f=GirlUnderground-FR.pdf
 pdfly cat $f 1:15 -o tmp.pdf
 pdfly 2-up tmp.pdf tmp-2up.pdf
-pdfly cat $f 0 tmp-2up.pdf $f 16: -o GirlUnderground-FR-2up.pdf
+pdfly cat $f 0 tmp-2up.pdf $f 15: -o GirlUnderground-FR-2up.pdf
 rm tmp.pdf tmp-2up.pdf
+
+if [ -r Girl_Underground.pdf ]; then
+  pdfly cat Girl_Underground.pdf 5:13 29:47 -o tmp2.pdf
+  pdfly 2-up tmp2.pdf GirlUnderground-ToPrint.pdf
+  rm tmp2.pdf
+fi
